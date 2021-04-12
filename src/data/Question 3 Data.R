67bac4839763b82f3aeb44ca0e7d29346ef26fb7
ianlightfoot <73260250+ianlightfoot@users.noreply.github.com>
2021-04-12 13:26:38 -0400

    Read/munge raw data

---
+++ src/data/Question 3 Data.R
@@ -0,0 +1,42 @@
+# Databricks notebook source
+install.packages("ggeffects")
+library(ggeffects)
+library(MASS)
+library(caret)
+library(e1071)
+library(randomForest)
+library(SparkR)
+library(carrier)
+library(sparklyr)
+library(dplyr)
+library(lubridate)
+library(mlflow)
+install_mlflow()
+
+SparkR::sparkR.session()
+sc <- spark_connect(method = "databricks")
+
+# COMMAND ----------
+
+constructors <- spark_read_csv(sc, path = "s3://columbia-gr5069-main/raw/constructors.csv")
+constructors <- as.data.frame(constructors)
+constructors <- constructors %>%
+  filter()
+
+# COMMAND ----------
+
+constructorResults <- spark_read_csv(sc, path = "s3://columbia-gr5069-main/raw/constructor_results.csv")
+constructorResults <- as.data.frame(constructorResults)
+
+# COMMAND ----------
+
+constructorStandings <- spark_read_csv(sc, path = "s3://columbia-gr5069-main/raw/constructor_standings.csv")
+constructorStandings <- as.data.frame(constructorStandings)
+
+# COMMAND ----------
+
+races <- spark_read_csv(sc, path = "s3://columbia-gr5069-main/raw/races.csv")
+races <- as.data.frame(races)
+
+# COMMAND ----------
+
