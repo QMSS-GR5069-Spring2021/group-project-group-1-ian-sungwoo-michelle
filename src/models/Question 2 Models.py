4bd8abd34859e07c3422f23c42fd66c86164de89
Sungwoo Park <62078257+spark331@users.noreply.github.com>
2021-04-14 22:25:22 -0400

    Update: .DS_Store README.md reports src

--- src/models/Question 2 Models.py
+++ src/models/Question 2 Models.py
@@ -114,16 +114,3 @@ predictions_final = spark.createDataFrame(X_test_preds)
 # COMMAND ----------
 
 predictions_final.write.option("header", "true").save('s3://group1-gr5069/processsed/preds_second_place.csv',mode="overwrite")
-
-# COMMAND ----------
-
-# MAGIC %md #### Save to RDS
-
-# COMMAND ----------
-
-predictions_final.write.format('jdbc').options(
-      url='jdbc:mysql://sp-gr5069.ccqalx6jsr2n.us-east-1.rds.amazonaws.com/sp_test',
-      driver='com.mysql.jdbc.Driver',
-      dbtable='second_place_preds',
-      user='admin',
-      password='VgCrEPeYKWaaIZZhoYHt').mode('overwrite').save()
