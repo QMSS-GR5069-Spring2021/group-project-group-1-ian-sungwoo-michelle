8d8932930fde4a102a4dad302683667741b148f6
ianlightfoot <73260250+ianlightfoot@users.noreply.github.com>
2021-04-14 20:54:01 -0400

    update code

--- src/data/Question 3 Data.R
+++ src/data/Question 3 Data.R
@@ -1,4 +1,9 @@
 # Databricks notebook source
+install.packages("ggeffects")
+install.packages("SparkR")
+install.packages("e1071")
+install.packages("carrier")
+install.packages("mlflow")
 library(ggeffects)
 library(MASS)
 library(caret)
@@ -34,6 +39,26 @@ constructorStandings <- as.data.frame(constructorStandings)
 
 races <- spark_read_csv(sc, path = "s3://columbia-gr5069-main/raw/races.csv")
 races <- as.data.frame(races)
+races
+
+# COMMAND ----------
+
+constructorMerge <- left_join(constructors, constructorResults, by = "constructorId")
+constructorMerge <- left_join(constructorMerge, constructorStandings, by = "constructorId")
+constructorMerge <- constructorMerge %>%
+  select(-raceId.x, -points.x) %>%
+  rename(raceId = raceId.y,
+        points = points.y)
+constructorMerge <- left_join(constructorMerge, races, by = "raceId")
+constructorMerge <- constructorMerge %>%
+  filter(year >= 1950, year <= 2010) %>%
+  select(constructorId, wins, position, points, nationality)
+constructorMerge <- constructorMerge[complete.cases(constructorMerge), ]
+
+# COMMAND ----------
+
+tbl <- copy_to(sc, constructorMerge, OVERWRITE=TRUE)
+spark_write_csv(tbl, 's3://group1-gr5069/interim/constructorMergeData')
 
 # COMMAND ----------
 
